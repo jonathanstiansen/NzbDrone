8ce803c66d938e5007d0d0d1a514bb4a32889723
Mark McDowall <markus.mcd5@gmail.com>
2013-05-06 19:32:43 -0700

    LINQ for paging missing episodes

--- NzbDrone.Core/History/HistoryRepository.cs
+++ NzbDrone.Core/History/HistoryRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using NzbDrone.Core.Datastore;
@@ -38,5 +39,7 @@ namespace NzbDrone.Core.History
 
             return null;
         }
+
+        //public List<History> GetPagedHistory() 
     }
 }
